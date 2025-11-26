e78fbf0f6324daa08c77b69b67d6e85be6db47ca
jgreenbaum <jake.greenbaum@mlb.com>
2025-11-26 12:59:00 -0800

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
+        fs: {
+          strict: false,
+        },
       },
       plugins: [react()],
       define: {
@@ -18,6 +22,10 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
       }
     };
 });
